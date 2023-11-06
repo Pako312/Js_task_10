2ed0d66af1ceade001eeb17a423e71b8e668114c
Timur <tima97855@gmail.com>
2023-11-06 14:15:49 +0600

    First task

--- index.js
+++ index.js
@@ -6,17 +6,17 @@
 // Например вызывая функцию getLongestCountryName(["Россия", "Украина", "Соединённые Штаты Америки"])
 // Ожидаемые результат: Соединённые Штаты Америки
 
-function findName(countryNames) {
-    let longestName = '';
-    for (let i = 0; i < countryNames.length; i++) {
-        if (countryNames[i].length > longestName.length) {
-            longestName = countryNames[i];
-        }
-    }
-    return longestName;
-}
-    const countryNames = ["Россия", "Украина", "Соединённые Штаты Америки"];
-const writeCountryName = findName(countryNames);
-console.log(writeCountryName);
+// function findName(countryNames) {
+//     let longestName = '';
+//     for (let i = 0; i < countryNames.length; i++) {
+//         if (countryNames[i].length > longestName.length) {
+//             longestName = countryNames[i];
+//         }
+//     }
+//     return longestName;
+// }
+//     const countryNames = ["Россия", "Украина", "Соединённые Штаты Америки"];
+// const writeCountryName = findName(countryNames);
+// console.log(writeCountryName);
 
 
